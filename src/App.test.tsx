ff12924478223434ab42a9d9c787efa91f979db7
phtn458 <phtn458@gmail.com>
2023-01-17 17:51:09 +0800

    updated testing with coverage summary

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,9 @@ test('<App /> renders', () => {
     const app = render(<App />);
     expect(app);
 });
+
+test('Title is React + TypeScript Template', () => {
+    const app = render(<App />);
+    const title = document.getElementById('title')
+    expect(title?.innerHTML).toBe('React + TypeScript Template')
+})
